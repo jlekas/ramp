527e92650ef5c835406e33872a516bef375f8589
jlekas <jlekas33@gmail.com>
2017-05-16 13:52:00 -0400

    fixing script to work on ubuntu need to do for mac also

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-#script to install python2.7.13 which is python version of program
-sudo apt-get install build-essential checkinstall
-sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-version=2.7.13
-cd ~/Downloads
-wget https://www.python.org/ftp/python/$version/Python-$version.tgz
-tar -xvf Python-$version.tgz
-cd Python-$version
-sudo ./configure
-sudo make altinstall
-# at this point python 2.7.13 is installed and can be used
-# in order to install packages used it's pip2 install <package name>
+#this is if the computer is ubuntu check python and packages installed
+if [ "$(uname)" == "Linux" ]; then
+  #script to install python2.7.13 which is python version of program
+  sudo apt-get install build-essential checkinstall
+  sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+  version=2.7.13
+  cd ~/Downloads
+  wget https://www.python.org/ftp/python/$version/Python-$version.tgz
+  tar -xvf Python-$version.tgz
+  cd Python-$version
+  sudo ./configure
+  sudo make altinstall
+  # at this point python 2.7.13 is installed and can be used
+  sudo apt-get install python-tk
+elif [ "$(uname)" == "Darwin" ]; then
+  #if the computer is a mac check that python 2.7.13 and various
+  #packages are installed in order to run application
+  echo "hello"
