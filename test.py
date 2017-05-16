72baed6cf49df8faf3cc38f066fbbe84b40637a9
shamal97 <shamal@Scotts-MBP-2.wv.cc.cmu.edu>
2017-05-16 12:53:13 -0400

    test.py is a example p2p application where a user can connect and be connected to, also its not working

---
+++ test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python  
+
+import sys
+import socket
+import thread
+
+#socket for receiving 
+recsock = socket.socket()    
+myname = socket.gethostname()
+recport = 1085
+recsock.bind((myname, recport))  
+
+s.listen(5)
+
+print myname 
+
+#socket for sending
+
+
+recsock.listen()
+
+while True:
+   peer, addr = recsock.accept()
+   print 'Got connection from', addr
+   peer.send('Thank you for connecting')
+
+
+
+   userin = raw_input()
+   if(userin == 'connect')
+     out = socket.socket()
+     sendname = raw_input('hostname:')
+     sendport = raw_input('port:')
+     sendport = int(sendport)
+     out.bind(sendname, sendport))
+     thread.start_new_thread(
+
+#def main():
+  
+
+
+
+#if __name__ == '__main__':
+#  main()
