028cc16ab3ee93e006e213c4ee79b578e5383ec8
jlekas <jlekas33@gmail.com>
2017-05-16 16:47:33 -0400

    fixing query object and db

--- cache.py
+++ cache.py
@@ -1,38 +1,44 @@
 #cache lookup by messageid containing dictionary of response and ip address
 #only want to store messageid for certain period of time
-#message id to be id different for all queries
-#could use database that stores messageid; num -1,1,2 indicating
-#not found last, on local, on friend; local path; friend ip it looked last time
+#message id to be id of message to see if gotten message fom other peer
+#different types of queries,
+#message is actual search either specific file or things looking for
+#could use database that stores messageid; num -1,1 indicating
+#not found last, on friend; friend ip it looked last time
 
 import os
 import sqlite3
 
-
 class query:
-    def __init__(self, messageID, userID):
+    def __init__(self, messageID, message):
         self.messageID = messageID
+        self.message = message
         self.userID = userID
 
     def __repr__(self):
         return ("query{message: "+self.messageID+", userId: "+self.userID+"}")
     
     def alreadySeen(self):
-        #returns 0 if not seen, -1 if seen and not found, 1 if found local,
-        #2 if found on other peer
+        #returns -1 if not seen, 0 if seen 
         #connect to database and check if seen
         sqlite_file = "Ramp.sqlite"
         db = sqlite3.connect(sqlite_file)
         cursor = db.cursor()
         #look in database to see if query seen already
         cursor.execute(
-            '''SELECT foundInt, localPath, hostName FROM queries where
-            messageID=?''', (self.messageID, )
+            '''SELECT * FROM queries where messageID=?''', (self.messageID, )
             )
         q = cursor.fetchone()
         db.close()
         if (q == None):
-            return 0
-        return q[1]
+            return -1
+        #if not seen add query to table
+        cursor.execute(
+            '''INSERT INTO queries(messageID) VALUES(?)''', (self.messageID, )
+            )
+        #returns fountInt
+        db.close()
+        return 0
 
     def displayFiles(self, path):
         if (os.path.isdir(path) == False):
@@ -43,28 +49,47 @@ class query:
                 files += self.displayFiles(path+ os.pathsep + file)
             return files
 
-    def publicLookup(self):
-        #if message was looked for and not found return -1 to indicate 
-        #that this peer looked for the query already and it/ its connections
-        #didn't have what was looked for
+    def findLocal(self, privPub):
+        #check if seen and if seen return -1 don't look in files
         seen = self.alreadySeen()
         if (seen == -1):
             return -1
         #query hasn't been seen so looks up query in public directory
         files = self.displayFiles("Public")
-        print(files)
-        return 0
+        if (privPub == "Private"):
+            files += self.displayFiles("Private")
+        #list of files that match query
+        retFiles = []
+        for file in files:
+            if self.message in file:
+                retFiles += [file]
+        #if no files match criteria return 0 else return list of filenames
+        if len(retFiles) == 0:
+            return 0
+        else:
+            return retFiles
 
-    def privateLookup(self):
-        #if message was looked for and not found return -1 to indicate 
-        #that this peer looked for the query already and it/ its connections
-        #didn't have what was looked for
+class fileQuery(query):
+
+    def __init__(self, messageID, message):
+        super().__init__(messageID, message)
+
+    def findLocal(self, privPub):
         seen = self.alreadySeen()
         if (seen == -1):
             return -1
         #query hasn't been seen so looks up query in public directory
         files = self.displayFiles("Public")
-        files += self.displayFiles("Private")
-        print(files)
-        return 0
+        if (privPub == "Private"):
+            files += self.displayFiles("Private")
+        #list of files that match query
+        retFiles = []
+        for file in files:
+            if self.message in file:
+                retFiles += [file]
+        #if no files match criteria return 0 else return list of filenames
+        if len(retFiles) == 0:
+            return 0
+        else:
+            return retFiles
 
--- query_db.py
+++ query_db.py
@@ -9,10 +9,7 @@ c = db.cursor()
 #make the queries table
 c.execute('''
         CREATE TABLE queries(
-            messageID varchar(255),
-            foundInt int,
-            localPath varchar(255),
-            hostName varchar(255)
+            messageID int
             );
         ''')
 db.commit()
