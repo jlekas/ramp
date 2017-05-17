96ff6816b4d3c0cc233088dd2b1bf15fcf7293b2
Scott <scotthamal1@gmail.com>
2017-05-17 18:20:22 -0400

    function for sending file

--- outgoing.py
+++ outgoing.py
@@ -12,3 +12,14 @@ def sendMessage(name,port,message):
     client.connect(addr)
     client.send(message)
     client.close()
+
+def sendFile(name,port,file) #file is a string with a file extension
+  port = int(port)
+  addr = (name,port)
+  buffer = 1024
+  data = open(file).read()
+  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  client.connect(addr)
+  client.send(data)
+  client.close()
